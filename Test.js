7fcca4df1127b50de1267ba8e25d9114716b9f64
Logity <Zaregotto307@gmail.com>
2023-11-14 18:14:38 +0200

    Table

--- Test.js
+++ Test.js
@@ -1,35 +1,70 @@
+const express = require('express');
 const cheerio = require('cheerio');
-const request = require('request');
+const axios = require('axios');
 
-// Посилання на ваші сторінки
-const urls = [
-    'https://steamcommunity.com/market/search?q=Kinetic%3A+Ambience+of+Reminiscence&descriptions=1#p1_price_asc',
-    'https://steamcommunity.com/market/search?descriptions=1&q=Kinetic%3A+Turbulent+Teleport#p1_price_asc',
-    'https://steamcommunity.com/market/search?descriptions=1&q=Kinetic%3A+When+Nature+Attacks#p1_price_asc'
-];
+const app = express();
+const PORT = 3000;
 
-// Функція для парсингу інформації з кожного посилання
-function parseUrl(urls) {
-    request(urls, (error, response, html) => {
-        if (!error && response.statusCode === 200) {
-            const $ = cheerio.load(html);
+const url = 'https://steamcommunity.com/market/search?descriptions=1&q=Kinetic%3A+Ambience+of+Reminiscence#p1_price_asc';
 
-            // Отримання інформації
-            const normalPrice = $('.normal_price').text();
-            const itemName = $('.market_listing_item_name_block').text();
-            const imageSrc = $('img').attr('src');
+let htmlTable = ''; // Зберігаємо останню відображену HTML-таблицю
 
-            // Виведення результатів
-            console.log(`URL: ${urls}`);
-            console.log(`Ціна: ${normalPrice}`);
-            console.log(`Назва предмету: ${itemName}`);
-            console.log(`Шлях до зображення: ${imageSrc}`);
-            console.log('---');
-        } else {
-            console.error(`Помилка при отриманні сторінки: ${urls}`);
-        }
-    });
+async function fetchDataAndRender() {
+    try {
+        const response = await axios.get(url);
+        const html = response.data;
+
+        const $ = cheerio.load(html);
+
+        const items = [];
+
+        const promises = $('.market_listing_row').map(async (index, element) => {
+            const name = $(element).find('.market_listing_item_name_block').text().trim();
+            const priceElement = $(element).find('.normal_price');
+
+            await new Promise(resolve => setTimeout(resolve, 1000));
+
+            const rawPrice = priceElement.length ? priceElement.text().trim() : 'N/A';
+            const price = parseFloat(rawPrice.replace(/[^\d.-]/g, ''));
+
+            items.push({ name, price });
+        }).get();
+
+        await Promise.all(promises);
+
+        // Сортування за ціною від найменшого до найбільшого
+        items.sort((a, b) => a.price - b.price);
+
+        const tableRows = items.map(item => `<tr><td>${item.name}</td><td>${item.price}</td></tr>`);
+        htmlTable = `
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${tableRows.join('')}
+                </tbody>
+            </table>
+        `;
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
 }
 
-// Виклик функції парсингу для кожного посилання
-urls.forEach(parseUrl);
+// Викликаємо функцію перший раз
+fetchDataAndRender();
+
+// Затемнення функції кожні 30 секунд
+setInterval(fetchDataAndRender, 30000);
+
+// Отправляем HTML-таблицу на клиент при запиті
+app.get('/', (req, res) => {
+    res.send(htmlTable);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
